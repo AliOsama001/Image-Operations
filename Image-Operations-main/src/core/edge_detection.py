edbf661cb7f39d378334bf4ee0422b0f88dca313
Ali Osama <ao4636867@gmail.com>
2026-04-30 17:20:57 +0300

    fix some issues

--- Image-Operations-main/src/core/edge_detection.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-import numpy as np
-import cv2
-def sobel_edge(image):
-   pass
--- Image-Operations-main/src/core/histogram.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import cv2
-def histogram_stretching(image):
-   pass
-
-
-def histogram_equalization(image):
-    pass 
