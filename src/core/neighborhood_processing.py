9488afbf06655acbeedcd5caef3fe1d9a1b5b325
Abdelrahman Maher <your-email@example.com>
2026-05-07 16:33:33 +0300

    filter methods

--- src/core/neighborhood_processing.py
+++ src/core/neighborhood_processing.py
@@ -2,22 +2,26 @@ import numpy as np
 import cv2
 import streamlit as st
 
-
 def average_filter(image, ksize):
-    pass 
+    
+    return cv2.blur(image, (ksize, ksize))
 
 
 def laplacian_filter(image):
-   pass
 
+    laplacian = cv2.Laplacian(image, cv2.CV_64F)
+    return np.uint8(np.absolute(laplacian))
 
 def median_filter(image, ksize):
-    pass 
 
+    return cv2.medianBlur(image, ksize)
 
 def max_filter(image, ksize):
-    pass 
 
+    kernel = np.ones((ksize, ksize), np.uint8)
+    return cv2.dilate(image, kernel)
 
 def min_filter(image, ksize):
-    pass 
+
+    kernel = np.ones((ksize, ksize), np.uint8)
+    return cv2.erode(image, kernel)
